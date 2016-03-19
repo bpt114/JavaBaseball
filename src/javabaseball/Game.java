9a05e2d75e5eb5bfb0eec610180ceccb74447d1a
Brian Thurmond <bpt114@gmail.com>
2016-03-19 19:21:52 -0400

    Update Game, Team

--- src/javabaseball/Game.java
+++ src/javabaseball/Game.java
@@ -6,64 +6,19 @@ package javabaseball;
  */
 public class Game {
     
-    public Game() {        
-    }
+    int inning = 0;
+    int myScore = 0;
+    int oppScore = 0;
+    int myBatter = 0;
+    int oppBatter = 0;
+    int balls = 0;
+    int strikes = 0;
+    int outs = 0;
     
     public void play() {
         
-        Player [] myTeam = new Player [9]; //Player array
-        Player [] myOpponent = new Player [9]; //opposing Player array
-        
-        for (int i = 0; i < 9; i++) { //create a team of nine Players
-            
-            if (i <= 2) {
-                Player myPlayer = new Player(1, i);
-                myTeam[i] = myPlayer;
-            }
-            
-            else if (i == 3) {
-                Player myPlayer = new Player(3, 3);
-                myTeam[i] = myPlayer;
-            }
-            
-             else if (i > 3 && i < 8 ) {
-                Player myPlayer = new Player(4, i);
-                myTeam[i] = myPlayer;
-            }
-            
-             else if (i == 8) {
-                Player myPlayer = new Player(8, 8);
-                myTeam[i] = myPlayer;
-            }
-        }
-        
-        for (int i = 0; i < 9; i++) { //create a team of nine opposing Players
-            
-            if (i >= 2) {
-                Player myPlayer = new Player(1, i);
-                myOpponent[i] = myPlayer;
-            }
-            
-            else if (i == 3) {
-                Player myPlayer = new Player(3, 3);
-                myOpponent[i] = myPlayer;
-            }
-            
-             else if (i > 3 && i < 8 ) {
-                Player myPlayer = new Player(4, i);
-                myOpponent[i] = myPlayer;
-            }
-            
-             else if (i == 8) {
-                Player myPlayer = new Player(8, 8);
-                myOpponent[i] = myPlayer;
-            }
-        }
-        
-        System.out.println("Southeast Montana State Lineup:");
-        for (int i = 0; i < myTeam.length; i++) {
-            Player tempPlayer = myTeam[i];
-            System.out.println("Position: " + tempPlayer.getPosition() + "  Average: " + tempPlayer.getBattingAverage());
-        }
+        Team myTeam = new Team("Bobcats");
+        Team myOpponent = new Team("Vipers");
+        myTeam.printTeam();        
     }
 }
--- src/javabaseball/Team.java
+++ src/javabaseball/Team.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package javabaseball;
 
 /**
@@ -12,4 +6,46 @@ package javabaseball;
  */
 public class Team {
     
+    String name;
+    int runs = 0;
+    int outs = 0;
+    Player [] myTeam;
+    
+    public Team(String teamName) {
+        
+        name = teamName;
+        myTeam = new Player [9];
+        
+        for (int i = 0; i < 9; i++) { //create a team of nine Players
+            
+            if (i <= 2) {
+                Player myPlayer = new Player(1, i);
+                myTeam[i] = myPlayer;
+            }
+            
+            else if (i == 3) {
+                Player myPlayer = new Player(3, 3);
+                myTeam[i] = myPlayer;
+            }
+            
+             else if (i > 3 && i < 8 ) {
+                Player myPlayer = new Player(4, i);
+                myTeam[i] = myPlayer;
+            }
+            
+             else if (i == 8) {
+                Player myPlayer = new Player(8, 8);
+                myTeam[i] = myPlayer;
+            }
+        }
+    }
+    
+    public void printTeam() {
+        
+        System.out.println("Team: " + name);
+        
+        for (int i = 0; i < myTeam.length; i++) {
+            System.out.println("Position: " + myTeam[i].myPosition + " Average: " + myTeam[i].battingAvg);
+        }
+    }
 }
